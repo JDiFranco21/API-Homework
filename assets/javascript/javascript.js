8041b9b2a55afe5337586a0abfeb3a9c5ab12f81
Joe <jdifranco21@gmail.com>
2018-01-20 20:27:23 -0600

    initial commit

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,8 @@
+var characterList = ["Darth Vader", "Luke Skywalker", "Han Solo", "Chewbacca", "C-3PO", "R2-D2", "Princess Leia", "Boba Fett", "Jabba the Hutt", "Yoda"];
+var character = "";
+var limit = 10;
+var oldlimit = 0;
+var animate = false;
+var response = "";
+
+var queryURL = "https://swapi.co/";
